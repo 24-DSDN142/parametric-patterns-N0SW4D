81ae2c1f34c0d6026ca311f270f2cbd451c1bedb
N0SW4D <howartdaws@myvuw.ac.nz>
2024-07-25 11:59:46 +1200

    Made a sword
    made a sword

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -19,5 +19,47 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+let bottomBlade = 60;
+let topBlade = 30; //less than 40
+let point = 10; //more than topblade
+let pomelSize = 10;
+
+fill (130,130,130);
+strokeWeight(1);
+
+  //blade
+  beginShape();
+  vertex(80, bottomBlade);
+  vertex(80, 50);
+  vertex(85, 45);
+  vertex(80, 40);
+  vertex(80, topBlade);
+  vertex(100, point);
+  vertex(120, topBlade);
+  vertex(120, 60);
+  vertex(118, 64);
+  vertex(120, 68);
+  vertex(120, bottomBlade);
+  endShape(CLOSE);
+
+  //crossguard
+  fill(252, 187, 88);
+  rect(60, bottomBlade, 80, 20);
+  rect (78, bottomBlade-5, 44, 5);
+
+  //handle
+  fill(120, 36, 17);
+  //rect(90, bottomBlade+20, 20, 40);
+  beginShape();
+  vertex(90, bottomBlade+20);
+  vertex(90, bottomBlade+40);
+  vertex(100, bottomBlade+60);
+  vertex(110, bottomBlade+40);
+  vertex(110, bottomBlade+20);
+  endShape(CLOSE);
+
+  //pomel
+  fill(252, 187, 88);
+  ellipse(100, bottomBlade+60, pomelSize, pomelSize);
+
 }
